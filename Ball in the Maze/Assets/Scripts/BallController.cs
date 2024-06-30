8b7caff5d3618379f1d322a51eb5e490feeede9d
harshaKARNATI20 <harshavardhanhasty@gmail.com>
2024-06-30 12:11:40 +0530

    Project

---
+++ Ball in the Maze/Assets/Scripts/BallController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    // GetAxis Method
+    // Time.deltaTime is a timeframe in seconds
+    public float speed;
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        // Debug.Log("Horizontal: " + horizontalInput + " Vertical: " + verticalInput);
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput) * speed * Time.deltaTime;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.AddForce(movement, ForceMode.Impulse);
+    }
+}
---
+++ Ball in the Maze/Assets/Scripts/Collectables.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectables : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ScoreManager.instance.AddScore(1);
+            Invoke("DestroyCollectables", 0.2f);
+            // Debug.Log("Ball Collided with coin");
+        }
+    }
+    void DestroyCollectables()
+    {
+        Destroy(this.gameObject);
+    }
+}
